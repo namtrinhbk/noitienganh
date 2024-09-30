2f6a9234ea90062666dc91bc664db8d0280907bf
namtrinhbk <namtrinh.bk@gmail.com>
2024-09-30 11:44:17 +0700

    sw.js

--- sw.js
+++ sw.js
@@ -1,11 +1,22 @@
-self.addEventListener('install', function(event) {
-  console.log('Service worker installing...');
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/script.js',
+        '/icon-192x192.png',
+        '/icon-512x512.png'
+      ]);
+    })
+  );
 });
 
-self.addEventListener('activate', function(event) {
-  console.log('Service worker activating...');
-});
-
-self.addEventListener('fetch', function(event) {
-  console.log('Fetching:', event.request.url);
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
